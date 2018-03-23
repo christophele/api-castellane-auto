90f1774c198b2cc8d3675ddedac8e298befe5bc6
cle <le_christophe@hotmail.fr>
2018-03-23 01:21:25 +0100

    Update

---
+++ routes/moniteur.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const mysqlConnexion = require('../server.js');
+
+router.get('/', function(req, res, next) {
+	mysqlConnexion.query('SELECT * FROM moniteur', function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
