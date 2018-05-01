14882aa04f90bba4b92c576ede6a7544cec17ccf
cle <le_christophe@hotmail.fr>
2018-05-01 15:14:36 +0200

    Update config.js

--- sql/config.js
+++ sql/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	host: '185.10.17.133',
-    user: 'christophe',
-    password: 'azerty',
-    database: 'castellaneauto'
+	host: 'reset',
+    user: 'reset',
+    password: 'reset',
+    database: 'reset'
 };
